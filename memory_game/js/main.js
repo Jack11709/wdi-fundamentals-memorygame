5d67995862338f64a86d6fe84746ad3066791e62
Jack May <jackmay@Jack-Mays-MacBook-Pro.local>
2017-04-16 15:21:02 +0100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,51 @@
+
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		image: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		image: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		image: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		image: "images/king-of-diamonds.png"
+	}
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		}
+		else {
+		alert("Sorry try again");
+		}
+	}
+}
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
+	console.log(cards[cardId].image);
+	console.log(cards[cardId].suit);
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
